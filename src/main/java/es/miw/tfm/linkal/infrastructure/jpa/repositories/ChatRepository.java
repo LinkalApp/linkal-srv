7fa687fa78d54c6679fd1f2685e5a599aed260a5
irenerlz <irenereciolopez12@gmail.com>
2026-06-16 22:42:49 +0200

    Classes & methods for obtaining the chats of the user LinkalApp/linkal#40

--- src/main/java/es/miw/tfm/linkal/infrastructure/jpa/repositories/ChatRepository.java
+++ src/main/java/es/miw/tfm/linkal/infrastructure/jpa/repositories/ChatRepository.java
@@ -2,10 +2,20 @@ package es.miw.tfm.linkal.infrastructure.jpa.repositories;
 
 import es.miw.tfm.linkal.infrastructure.jpa.entities.ChatEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ChatRepository extends JpaRepository<ChatEntity, UUID> {
     Optional<ChatEntity> findByMatch_Id(UUID matchId);
+
+    @Query("""
+            SELECT c FROM ChatEntity c
+            WHERE (c.match.influencer.id = :userId OR c.match.campaign.business.id = :userId)
+              AND c.match.status = 'COMPLETED'
+            """)
+    List<ChatEntity> findAllByUserId(@Param("userId") UUID userId);
 }
